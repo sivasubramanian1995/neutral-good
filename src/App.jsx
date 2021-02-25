2adaa42194521fcf456b26cddd7fcd3ccf090d6d
Siva Subramanian R <siva.subramanian@krds.fr>
2021-02-25 12:53:10 +0530

    section 2

--- src/App.jsx
+++ src/App.jsx
@@ -84,10 +84,94 @@ const App = () => (
           </div>
         </div>
         <div className="section">
-          <div className="foursection" />
-          <div className="foursection" />
-          <div className="foursection" />
-          <div className="foursection" />
+          <div className="foursection">
+            <div className="fs-container">
+              <div className="fs-head">
+                <div className="fs-head-title">
+                  <div className="fs-head-bold">Santander</div>
+                  <div className="fs-head-card">
+                    <span className="fs-cardno">88 **** 9448</span>
+                    <img className="fs-cardcopy" src="/images/copy.svg" alt="Copy" />
+                  </div>
+                </div>
+                <div className="fs-head-logo"><img src="/images/sant.png" alt="Santander" /></div>
+              </div>
+              <div className="fs-footer-cash">
+                <div>
+                  <span className="fs-cash">12.220,</span>
+                  <span className="fs-cash-seggr">65</span>
+                  <span className="fs-cash-currency">$</span>
+                </div>
+                <div className="fs-footer-menu"><img src="/images/menu.svg" alt="menu" /></div>
+              </div>
+            </div>
+          </div>
+          <div className="foursection">
+            <div className="fs-container">
+              <div className="fs-head">
+                <div className="fs-head-title">
+                  <div className="fs-head-bold">CitiBank</div>
+                  <div className="fs-head-card">
+                    <span className="fs-cardno">45 **** 8854</span>
+                    <img className="fs-cardcopy" src="/images/copy.svg" alt="Copy" />
+                  </div>
+                </div>
+                <div className="fs-head-logo"><img src="/images/sant.png" alt="CitiBank" /></div>
+              </div>
+              <div className="fs-footer-cash">
+                <div>
+                  <span className="fs-cash">25.070,</span>
+                  <span className="fs-cash-seggr">65</span>
+                  <span className="fs-cash-currency">$</span>
+                </div>
+                <div className="fs-footer-menu"><img src="/images/menu.svg" alt="menu" /></div>
+              </div>
+            </div>
+          </div>
+          <div className="foursection">
+            <div className="fs-container">
+              <div className="fs-head">
+                <div className="fs-head-title">
+                  <div className="fs-head-bold">Deutsche bank</div>
+                  <div className="fs-head-card">
+                    <span className="fs-cardno">67 **** 6821</span>
+                    <img className="fs-cardcopy" src="/images/copy.svg" alt="Copy" />
+                  </div>
+                </div>
+                <div className="fs-head-logo"><img src="/images/sant.png" alt="CitiBank" /></div>
+              </div>
+              <div className="fs-footer-cash">
+                <div>
+                  <span className="fs-cash">570,</span>
+                  <span className="fs-cash-seggr">00</span>
+                  <span className="fs-cash-currency">$</span>
+                </div>
+                <div className="fs-footer-menu"><img src="/images/menu.svg" alt="menu" /></div>
+              </div>
+            </div>
+          </div>
+          <div className="foursection">
+            <div className="fs-container">
+              <div className="fs-head">
+                <div className="fs-head-title">
+                  <div className="fs-head-bold">Credit Agricole</div>
+                  <div className="fs-head-card">
+                    <span className="fs-cardno">55 **** 7655</span>
+                    <img className="fs-cardcopy" src="/images/copy.svg" alt="Copy" />
+                  </div>
+                </div>
+                <div className="fs-head-logo"><img src="/images/sant.png" alt="CitiBank" /></div>
+              </div>
+              <div className="fs-footer-cash">
+                <div>
+                  <span className="fs-cash">2.680,</span>
+                  <span className="fs-cash-seggr">50</span>
+                  <span className="fs-cash-currency">$</span>
+                </div>
+                <div className="fs-footer-menu"><img src="/images/menu.svg" alt="menu" /></div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="section">
           <div className="transactionsection" />
